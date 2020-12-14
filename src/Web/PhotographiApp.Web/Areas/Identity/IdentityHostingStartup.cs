40896a921fcafbe44796ff1bed98f76551f60087
Tsvetan Kulenski <tsvetan@kulenski.eu>
2020-12-14 22:49:59 +0200

    UI Improvements (part 3). Identity pages refactor. Comments, Topics, PhotoThumbnail.

---
+++ src/Web/PhotographiApp.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(PhotographiApp.Web.Areas.Identity.IdentityHostingStartup))]
+
+namespace PhotographiApp.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
---
+++ src/Web/PhotographiApp.Web/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,98 @@
+﻿namespace PhotographiApp.Web.Areas.Identity.Pages.Account.Manage
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using PhotographiApp.Data.Models.Application;
+
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public IndexModel(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public string Username { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Phone]
+            [Display(Name = "Phone number")]
+            public string PhoneNumber { get; set; }
+        }
+
+#pragma warning disable SA1201 // Elements should appear in the correct order
+        private async Task LoadAsync(User user)
+#pragma warning restore SA1201 // Elements should appear in the correct order
+        {
+            var userName = await this.userManager.GetUserNameAsync(user);
+            var phoneNumber = await this.userManager.GetPhoneNumberAsync(user);
+
+            this.Username = userName;
+
+            this.Input = new InputModel
+            {
+                PhoneNumber = phoneNumber,
+            };
+        }
+
+#pragma warning disable SA1202 // Elements should be ordered by access
+        public async Task<IActionResult> OnGetAsync()
+#pragma warning restore SA1202 // Elements should be ordered by access
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            await this.LoadAsync(user);
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            if (user == null)
+            {
+                return this.NotFound($"Unable to load user with ID '{this.userManager.GetUserId(this.User)}'.");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                await this.LoadAsync(user);
+                return this.Page();
+            }
+
+            var phoneNumber = await this.userManager.GetPhoneNumberAsync(user);
+            if (this.Input.PhoneNumber != phoneNumber)
+            {
+                var setPhoneResult = await this.userManager.SetPhoneNumberAsync(user, this.Input.PhoneNumber);
+                if (!setPhoneResult.Succeeded)
+                {
+                    this.StatusMessage = "Unexpected error when trying to set phone number.";
+                    return this.RedirectToPage();
+                }
+            }
+
+            await this.signInManager.RefreshSignInAsync(user);
+            this.StatusMessage = "Your profile has been updated";
+            return this.RedirectToPage();
+        }
+    }
+}
